1ce4acea02c7452e997fdce6e5bdae6c4c8f608c
soroushamdg <soroushmpour@gmail.com>
2022-05-13 19:37:40 +0430

    Initial Commit

---
+++ todox/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todox/route/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.fadeIn,
+      getPages: Routes.pages,
+    );
+  }
+}
