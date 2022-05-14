a28eb5ec22e06ae63477da7ea09f9d86e1e1471b
soroushamdg <soroushmpour@gmail.com>
2022-05-14 12:18:29 +0430

    added task screen route.

--- todox/lib/routes/routes.dart
+++ todox/lib/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
+import 'package:todox/page/add_task.dart';
 import 'package:todox/page/home.dart';
 
 class Routes {
   static List<GetPage> get pages => [
         GetPage(name: '/home', page: () => HomeScreen()),
+        GetPage(name: '/add', page: () => AddTaskScreen()),
       ];
 }
